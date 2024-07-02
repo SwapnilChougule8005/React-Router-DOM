901473e05583c006614bcd1e122bef09d9fc2a82
SwapnilChougule8005 <157900311+SwapnilChougule8005@users.noreply.github.com>
2024-07-02 17:44:48 +0530

    Add files via upload

---
+++ JavascriptB1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const BatchOne = () => {
+    return (
+        <div style={{textAlign:"center"}}>
+          <h2>Batch first </h2>
+          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNIwN48oq2JgeCpv7hkavw8flFTDEOfLkyKw&s'/>
+          <p>time 03:00pm to 06:00pm</p>
+          <p>
+              Join my free live online JavaScript classes. , main website : https://sandkdesignstudio.in/fcc/, discord : https://discord.gg/CVugpJRBTB, ----, About me, Freelance Tutor - Coding, Generative Art ...
+          </p>
+        </div>
+    )
+}
+export default BatchOne;
